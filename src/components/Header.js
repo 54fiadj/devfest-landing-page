e4351dbc9136d626ea0a025c3c86d845d3a648ae
54fiadj <54fiadj@gmail.com>
2023-12-10 00:25:32 +0100

    title animation complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,8 @@ import React from 'react';
 import illustrationSet1 from '../images/illustration-set-1.svg';
 import illustrationSet2 from '../images/illustration-set-2.svg';
 import devfestTitle from '../images/devfest-title.svg';
+import puzzle1 from '../images/puzzle-piece-1.svg';
+import puzzle2 from '../images/puzzle-piece-2.svg';
 import calendar from '../images/calendar.svg';
 import location from '../images/location.svg';
 
@@ -10,7 +12,13 @@ const Header = () => {
     <div>
         <div className='devfest-title'>
             <img className='doodles1' src={illustrationSet1} alt="doodles" />
-            <img className='devfest-anim' src={devfestTitle} alt="devfest 2023" />
+            <div className='devfest-anim'>
+              <img className='puzzle1' src={puzzle1} alt="" />
+              <div className='title-container'>
+                <img className='title' src={devfestTitle} alt="" />
+              </div>
+              <img className='puzzle2' src={puzzle2} alt="" />
+            </div>
             <img className='doodles2' src={illustrationSet2} alt="doodles" />
         </div>
         <div className='date-location'>
